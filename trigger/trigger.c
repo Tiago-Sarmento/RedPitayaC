f7ff1e56cd4424b70ff851eafad041000b2cf86d
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-04-06 16:13:04 +0100

    Update trigger.c

--- trigger/trigger.c
+++ trigger/trigger.c
@@ -135,7 +135,7 @@ int main(void)
  			printf("failedB pulse\n");
  		} else{ printf("goodB pulse\n");}
 		printf("counterA = %i\n",counterA);
-		//sprintf(payload,"{\"messageid\": 1345, \"value\": %d, \"timestamp\": %lu}",counterA,(unsigned long)time(NULL));
+		sprintf(payload,"{\"messageid\": 1345, \"value\": %d, \"timestamp\": %lu}",counterA,(unsigned long)time(NULL));
 		//sprintf(payload,"&d",counterA);
 		//mqtt_send("message in a bottle");
 		printf("counterB = %i\n",counterB);
