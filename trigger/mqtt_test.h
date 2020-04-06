b33ba7e4202b737c4dab35c31ccc29feb6fe5968
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-04-06 10:23:27 +0100

    Create mqtt_test.h

---
+++ trigger/mqtt_test.h
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "MQTTClient.h"
+
+#define ADDRESS     "test.mosquitto.org"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "MakerIOTopicReceipt"
+//#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
+
+int main(int argc, char* argv[], char* payload[]);
