854b192f1fbd24a1c40fcdb77b35a43e8244c594
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-04-07 08:45:08 +0100

    Update trigger.c

--- trigger/trigger.c
+++ trigger/trigger.c
@@ -62,89 +62,92 @@ int main(void)
 	/***************************/
 	/** MAIN ACQUISITION LOOP **/
 	/***************************/
-	for (trace_counts=0; trace_counts<100; trace_counts++)
+	for(twosecondloopes=0; twosecondloops<5;twosecondloops++)
 	{
-		/*Set trigger, begin acquisition when condition is met*/
-		osc_fpga_arm_trigger(); //start acquiring, incrementing write pointer
-		osc_fpga_set_trigger(0x6); // where do you want your triggering from?
-		/*    0 - end of acquisition/no acquisition
-		*     1 - trig immediately
-		*     2 - ChA positive edge 
-		*     3 - ChA negative edge
-		*     4 - ChB positive edge 
-		*     5 - ChB negative edge
-		*     6 - External trigger 0
-		*     7 - External trigger 1*/
-		// Trigger always changes to 0 after acquisition is completed, write pointer stops incrementing
-		//->fpga.osc.h l66
-
-
-		/*Wait for the acquisition to finish = trigger is set to 0*/
-		trig_test=(g_osc_fpga_reg_mem->trig_source); // it gets the above trigger value
-		// if acquistion is not yet completed it should return the number you set above and 0 otherwise
-		while (trig_test!=0) // with this loop the program waits until the acquistion is completed before cont.
+		for (trace_counts=0; trace_counts<100; trace_counts++)
 		{
-			trig_test=(g_osc_fpga_reg_mem->trig_source);
+			/*Set trigger, begin acquisition when condition is met*/
+			osc_fpga_arm_trigger(); //start acquiring, incrementing write pointer
+			osc_fpga_set_trigger(0x6); // where do you want your triggering from?
+			/*    0 - end of acquisition/no acquisition
+			*     1 - trig immediately
+			*     2 - ChA positive edge 
+			*     3 - ChA negative edge
+			*     4 - ChB positive edge 
+			*     5 - ChB negative edge
+			*     6 - External trigger 0
+			*     7 - External trigger 1*/
+			// Trigger always changes to 0 after acquisition is completed, write pointer stops incrementing
+			//->fpga.osc.h l66
+
+
+			/*Wait for the acquisition to finish = trigger is set to 0*/
+			trig_test=(g_osc_fpga_reg_mem->trig_source); // it gets the above trigger value
+			// if acquistion is not yet completed it should return the number you set above and 0 otherwise
+			while (trig_test!=0) // with this loop the program waits until the acquistion is completed before cont.
+			{
+				trig_test=(g_osc_fpga_reg_mem->trig_source);
+			}
+			//->fpga_osc.c l366
+
+
+			trig_ptr = g_osc_fpga_reg_mem->wr_ptr_trigger; // get pointer to mem. adress where trigger was met
+			//->fpga_osc.c l283
+			osc_fpga_get_sig_ptr(&cha_signal, &chb_signal); 
+			//->fpga_osc.c l378
+
+
+			/*now read N samples from the trigger pointer location.*/
+			int i;
+			int ptr;
+			int counterA = 0;
+			int counterB = 0;
+			for (i=0; i < N; i++) {
+				ptr = (trig_ptr+i)%BUF;
+
+				if (cha_signal[ptr]>=8192){ // properly display negative values fix
+					//        		printf("%d ",cha_signal[ptr]-16384);
+					//fprintf(fp, "%d, ", cha_signal[ptr]-16384);
+
+				}
+				else{
+					//        		printf("%d ",cha_signal[ptr]);
+					//fprintf(fp, "%d, ", cha_signal[ptr]);;
+					if(cha_signal[ptr] > 500){
+						counterA++;
+					}
+				}
+				if (chb_signal[ptr]>=8192){ // properly display negative values fix
+					//        		printf("%d ",cha_signal[ptr]-16384);
+					//fprintf(fp, "%d\n", chb_signal[ptr]-16384);
+				}
+				else{
+					//        		printf("%d ",cha_signal[ptr]);
+					//fprintf(fp, "%d\n", chb_signal[ptr]);;
+					if(chb_signal[ptr] > 500){
+						counterB++;
+					}
+				}  
+
+				}
+			if (counterA < 3000){
+				printf("failedA pulse\n");
+			} else{ printf("goodA pulse\n"); }
+			if (counterB < 3000){
+				printf("failedB pulse\n");
+			} else{ printf("goodB pulse\n");}
+			printf("counterA = %i\n",counterA);
+			//char *payloadMain[];
+			//sprintf(payloadMain,"{\"messageid\": 1345, \"value\": %d, \"timestamp\": %u}",counterA,(unsigned)time(NULL));
+			sprintf(payload,"%d",counterA);
+
+			printf("counterB = %i\n",counterB);
+			fprintf(fp, "\n");
+			printf("iteration = %i", trace_counts);
+			printf("time is = %u\n", (unsigned)time(NULL));
 		}
-		//->fpga_osc.c l366
-
-
-		trig_ptr = g_osc_fpga_reg_mem->wr_ptr_trigger; // get pointer to mem. adress where trigger was met
-		//->fpga_osc.c l283
-		osc_fpga_get_sig_ptr(&cha_signal, &chb_signal); 
-		//->fpga_osc.c l378
-
-
-		/*now read N samples from the trigger pointer location.*/
-		int i;
-		int ptr;
-		int counterA = 0;
-		int counterB = 0;
-		for (i=0; i < N; i++) {
-			ptr = (trig_ptr+i)%BUF;
-
-			if (cha_signal[ptr]>=8192){ // properly display negative values fix
-				//        		printf("%d ",cha_signal[ptr]-16384);
-				//fprintf(fp, "%d, ", cha_signal[ptr]-16384);
-				
-			}
-			else{
-				//        		printf("%d ",cha_signal[ptr]);
-				//fprintf(fp, "%d, ", cha_signal[ptr]);;
- 				if(cha_signal[ptr] > 500){
- 					counterA++;
- 				}
-			}
- 			if (chb_signal[ptr]>=8192){ // properly display negative values fix
- 				//        		printf("%d ",cha_signal[ptr]-16384);
- 				//fprintf(fp, "%d\n", chb_signal[ptr]-16384);
- 			}
- 			else{
- 				//        		printf("%d ",cha_signal[ptr]);
- 				//fprintf(fp, "%d\n", chb_signal[ptr]);;
- 				if(chb_signal[ptr] > 500){
- 					counterB++;
- 				}
- 			}  
-
-			}
-		if (counterA < 3000){
-			printf("failedA pulse\n");
-		} else{ printf("goodA pulse\n"); }
- 		if (counterB < 3000){
- 			printf("failedB pulse\n");
- 		} else{ printf("goodB pulse\n");}
-		printf("counterA = %i\n",counterA);
-		//char *payloadMain[];
-		//sprintf(payloadMain,"{\"messageid\": 1345, \"value\": %d, \"timestamp\": %u}",counterA,(unsigned)time(NULL));
-		sprintf(payload,"%d",counterA);
-		//mqtt_send(payload);
-		printf("counterB = %i\n",counterB);
-		fprintf(fp, "\n");
-		printf("iteration = %i", trace_counts);
-		printf("time is = %u\n", (unsigned)time(NULL));
 	}
-
+		//mqtt_send();
 	// cleaning up all nice like mommy taught me
 	fclose(fp);
 	osc_fpga_exit();
