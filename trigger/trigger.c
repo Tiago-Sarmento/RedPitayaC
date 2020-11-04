c15e7c5eb9d880f0d1b44a01870348495d54d811
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-11-04 12:59:13 +0000

    Update trigger.c

--- trigger/trigger.c
+++ trigger/trigger.c
@@ -141,9 +141,9 @@ int main(void)
 				printf("failedB pulse\n");
 			} else{ printf("goodB pulse\n");}
 			printf("counterA = %i\n",counterA);
-			char *payloadMain[100]; // allocate excessive memory to avoid memory problems
+			char payloadMain[100]; // allocate excessive memory to avoid memory problems
 			// sprintf copies string into variable
-			//sprintf(payloadMain,"{\"messageid\": 1345, \"value\": %d, \"timestamp\": %u}",counterA,(unsigned)time(NULL));
+			sprintf(payloadMain,"{\"messageid\": 1345, \"value\": %d, \"timestamp\": %u}",counterA,(unsigned)time(NULL));
 			sprintf(payload,"%d",counterA);
 			mqtt_send(payloadMain); // can run at 50Hz without interrupting script
 			printf("counterB = %i\n",counterB);
