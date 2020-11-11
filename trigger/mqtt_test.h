a9231a0563bc2140304154d4c32945228ab5738c
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-11-11 10:09:58 +0000

    add second MQTT topic

--- trigger/mqtt_test.h
+++ trigger/mqtt_test.h
@@ -5,11 +5,13 @@
 
 #define ADDRESS     "icxparrot.isis.rl.ac.uk" //"test.mosquitto.org"   // choose a mqtt broker, such as broker.hive.com
 #define CLIENTID    "ExampleClientPub" // unique identifier for the client, can be whatever you like
-#define TOPIC       "/vista/values/failed_pulse_count"//"MakerIOTopicReceipt" // topic to send message to
+#define TOPIC_A       "/vista/values/failed_pulse_count"//"MakerIOTopicReceipt" // topic to send message to
+#define TOPIC_B       "/vista/values/failed_pulse_count2"//"MakerIOTopicReceipt" // topic to send messag
 //#define PAYLOAD     "Hello World!"
 #define QOS         1 // quality of service - 0 means fire and forget - change to suit speed needs
 #define TIMEOUT     10000L
 
 void mqtt_connect(void);
 void mqtt_disconnect(void);
-int mqtt_send(char *payload);
+int mqtt_sendA(char *payload);
+int mqtt_sendB(char *payload);
