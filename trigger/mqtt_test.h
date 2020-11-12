311fc1c98abaf57808adb29e4c3c49f06ad80a78
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-11-12 14:30:20 +0000

    Update mqtt_test.h

--- trigger/mqtt_test.h
+++ trigger/mqtt_test.h
@@ -5,8 +5,8 @@
 
 #define ADDRESS     "icxparrot.isis.rl.ac.uk" //"test.mosquitto.org"   // choose a mqtt broker, such as broker.hive.com
 #define CLIENTID    "ExampleClientPub" // unique identifier for the client, can be whatever you like
-#define TOPIC_A       "/vista/values/failed_pulse_count"//"MakerIOTopicReceipt" // topic to send message to
-#define TOPIC_B       "/vista/values/failed_pulse_count2"//"MakerIOTopicReceipt" // topic to send messag
+#define TOPIC_A       "vista/set/vespa/failed_pulse_count/value"//"MakerIOTopicReceipt" // topic to send message to
+#define TOPIC_B       "vista/set/vespa/failed_pulse_count2/value"//"MakerIOTopicReceipt" // topic to send messag
 //#define PAYLOAD     "Hello World!"
 #define QOS         1 // quality of service - 0 means fire and forget - change to suit speed needs
 #define TIMEOUT     10000L
