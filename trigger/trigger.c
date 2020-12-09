58fa90ec989ce5d84b2812a971312e2e378e82d3
Tiago-Sarmento <58564308+Tiago-Sarmento@users.noreply.github.com>
2020-12-09 14:02:44 +0000

    Update trigger.c

--- trigger/trigger.c
+++ trigger/trigger.c
@@ -122,7 +122,7 @@ int main(void)
 				else{ // points with value below 8192 are positive: 0 to 8192 maps 0 to 10V
 			   		//printf("%d ",cha_signal[ptr]);
 					//fprintf(fp, "%d, ", cha_signal[ptr]);;
-					if(cha_signal[ptr] > 500){ 
+					if(cha_signal[ptr] > 700){ 
 						counterA++;
 					}
 				}
